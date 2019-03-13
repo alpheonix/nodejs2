38fe914d5eb17845c43c09df3c81bf38f200a635
alpheonix <arthurblanc98@gmail.com>
2019-03-13 15:21:44 +0100

    3.3

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'node App' });
 });
 
-router.post('/ville',function(req, res) {
+router.post('/ville', function (req, response) {
+  console.log(req.body.nom_ville);
 
-  res.render("ville",{ ville: req.body.nom_ville,desc: req.body.desc});
+  request(`https://geocode.xyz/${req.body.nom_ville}?json=1&auth=387209274444184739495x1963`, { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    console.log(body.longt);
+    console.log(body.latt);
+
+    response.render("ville", { ville: req.body.nom_ville, desc: req.body.desc, longt: body.longt, latt: body.latt });
+  });
+  
+
 });
+
 
 module.exports = router;
